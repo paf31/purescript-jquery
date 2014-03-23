e6d0047f58e31b92b2de92cefd5a5e5cf4eab19c
krdlab <krdlab@gmail.com>
2014-03-23 18:52:23 +0900

    Update examples in order to correct a compile error.

--- examples/test.purs.hs
+++ examples/test.purs.hs
@@ -2,29 +2,29 @@ module Main where
 
 import Data.Either
 import Prelude
-import Data.JSON
+import Data.Foreign
 import Control.Monad.Eff
-import Control.Monad.JQuery
+import qualified Control.Monad.JQuery as J
 import Debug.Trace
 
 main = do
   -- Get the document body
-  b <- body
+  b <- J.body
 
   -- Create a text box
-  div <- create "<div>"
-  input <- create "<input>"
-  "Your Name: " `appendText` div
-  input `append` div
-  div `append` b
+  div <- J.create "<div>"
+  input <- J.create "<input>"
+  "Your Name: " `J.appendText` div
+  input `J.append` div
+  div `J.append` b
 
   -- Create a paragraph to display a greeting
-  greeting <- create "<p>"
-  { color: "red" } `css` greeting
-  greeting `append` b
+  greeting <- J.create "<p>"
+  { color: "red" } `J.css` greeting
+  greeting `J.append` b
 
   -- Listen for change events on the text box
-  flip (on "change") input $ do
-    Right name <- runParser readJSON <$> getValue input
+  flip (J.on "change") input $ do
+    Right name <- parseForeign read <$> J.getValue input
     trace $ "Name changed to " ++ name
-    setText ("Hello, " ++ name) greeting
+    J.setText ("Hello, " ++ name) greeting
